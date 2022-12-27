0fedd44053ebfdf2ac4989a7a93e97504a200a85
k8s <k8s@scott-tap-jump.tanzu.lab>
2022-12-27 17:24:25 +0000

    fix formatting

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -19,6 +19,9 @@ package org.springframework.samples.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 /**
  * PetClinic Spring Boot Application.
